e0a7e5abd0cca7c00e8302efe01bfd8040b6cfe9
bweston6 <35574350+bweston6@users.noreply.github.com>
2020-11-03 10:58:00 +0000

    Adding COMP105 assessment 1.

---
+++ COMP105/Assessments/Assignment1.hs
@@ -0,0 +1,48 @@
+-- Do not alter the following line
+module Assignment1 (char_to_int, repeat_char, decode, int_to_char, length_char, drop_char, encode, complex_encode, complex_decode) where
+
+
+-- Part A
+
+char_to_int :: Char -> Integer
+char_to_int = error "Not implemented"
+
+
+repeat_char :: Char -> Integer -> String
+repeat_char = error "Not implemented"
+
+
+decode :: String -> String
+decode = error "Not implemented"
+
+
+
+-- Part B
+
+int_to_char :: Integer -> Char
+int_to_char = error "Not implemented"
+
+
+length_char :: Char -> String -> Integer
+length_char = error "Not implemented"
+
+
+drop_char :: Char -> String -> String
+drop_char = error "Not implemented"
+
+
+encode :: String -> String
+encode = error "Not implemented"
+
+
+
+-- Part C
+
+complex_encode :: String -> String
+complex_encode = error "Not implemented"
+
+
+complex_decode :: String -> String
+complex_decode = error "Not implemented"
+
+
