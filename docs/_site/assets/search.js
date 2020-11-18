c6169c1c60d4fb26bb718ec9dd578cb2a5457290
bweston6 <35574350+bweston6@users.noreply.github.com>
2020-11-18 16:38:56 +0000

    Removing files and changing config.

--- docs/_site/assets/search.js
+++ docs/_site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"UoL/2018/06/01/header-image.html"},{'title':"Welcome",'url':"UoL/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
