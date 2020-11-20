7c2a1126816ade97408320c583a2d938cc879a1f
bweston6 <35574350+bweston6@users.noreply.github.com>
2020-11-20 18:57:02 +0000

    Fixing final formatting errors.

--- docs/_site/assets/COMP105/Lectures/2020-10-14.hs
+++
@@ -1,7 +0,0 @@
-addTwo x = x + 2
-addSquares x y = x**2 + y**2
-maxThree x y z = max (max x y) z
-pow2 x = 2^x -- raises input as a power of 2
-bin_to_dec b1 b2 = b1 * pow2 1 + b2 * pow2 0 
-{- uses the previous function to convert a two bit binary number to
- - a decimal -}
