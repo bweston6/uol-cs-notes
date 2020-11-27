dd201686c9727f021d5898dcc0ed0fa3a38eca34
bweston6 <35574350+bweston6@users.noreply.github.com>
2020-11-27 23:02:57 +0000

    Completing parts A, B and C.

--- COMP105/Assessments/Assessment 2/Assignment2.hs
+++ COMP105/Assessments/Assessment 2/Assignment2.hs
@@ -19,42 +19,46 @@ test_log = [('B', 100, 1104,  "VTI",  1),
 
 
 transaction_to_string :: Transaction -> String
-transaction_to_string = error "Not implemented"
+transaction_to_string (a, b, c, d, e) = let
+                                                        bought = case a of 'B' -> "Bought"
+                                                                           'S' -> "Sold"
+                                                in
+                                                        bought ++ " " ++ show b ++ " units of " ++ d ++ " for " ++ show c ++ " pounds each on day " ++ show e
 
 
 trade_report_list :: [Transaction] -> [String]
-trade_report_list = error "Not implemented"
+trade_report_list list = map transaction_to_string list
 
 
 stock_test :: String -> Transaction -> Bool
-stock_test = error "Not implemented"
+stock_test string (_,_,_,y,_)
+        | string == y    = True
+        | otherwise = False
 
 
 get_trades :: String -> [Transaction] -> [Transaction]
-get_trades = error "Not implemented"
+get_trades string list = filter (\ (_,_,_,tradeName,_) -> tradeName == string) list
 
 
 trade_report :: String -> [Transaction] -> String
-trade_report = error "Not implemented"
-
+trade_report string x = unlines $ trade_report_list $ get_trades string x
 
 
 -- Part B
 
 
 update_money :: Transaction -> Int -> Int
-update_money = error "Not implemented"
+update_money (x,y,z,_,_) purse
+        | x == 'B'  = purse - y * z
+        | otherwise = purse + y * z
 
 
 profit :: [Transaction] -> String -> Int
-profit = error "Not implemented"
-
+profit list string = foldr (\ x acc -> update_money x acc ) 0 (get_trades string list)
 
 
 profit_report :: [String] -> [Transaction] -> String
-profit_report = error "Not implemented"
-
-
+profit_report stringList list = unlines (zipWith (\ x y -> y ++ ": " ++ show x ) (map (profit list) stringList) stringList)
 
 
 -- Part C
@@ -73,9 +77,21 @@ test_prices = [
                 ("IWRD", [207, 211, 213, 221, 221, 222, 221, 218, 226, 234, 229, 229, 228, 222, 218, 223, 222, 218, 214])
               ]
 
+list_stock_name :: Prices -> [String]
+list_stock_name prices = map fst prices
 
+price_finder :: String -> Int -> Prices -> Int
+price_finder string day prices = case (head (filter (\ (x,_) -> x == string ) prices)) of (_,x) -> x !! (day - 1)
+
+string_to_trade_log :: String -> Prices -> [Transaction]
+string_to_trade_log string prices =
+        let 
+                parsed        = map words . lines $ string
+                string_to_int = (\ x -> read x :: Int)
+        in
+                map (\ [a,b,c,d] -> ((if a == "BUY" then 'B' else 'S'),(string_to_int b),(price_finder c (string_to_int d) prices),c,(string_to_int d))) parsed
 
 complex_profit_report :: String -> Prices -> String
-complex_profit_report = error "Not implemented"
-        
+-- complex_profit_report = error "Not implemented"
+complex_profit_report string prices = profit_report (list_stock_name prices) (string_to_trade_log string prices)
 
