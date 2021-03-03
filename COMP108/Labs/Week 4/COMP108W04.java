01c2155814492977d1e2101dcce90802c981d8d5
bweston6 <35574350+bweston6@users.noreply.github.com>
2021-03-03 10:49:03 +0000

    Adding resources for lab 4.

---
+++ COMP108/Labs/Week 4/COMP108W04.java
@@ -0,0 +1,31 @@
+//
+// Enter your name:
+// Enter your student ID:
+//
+import java.util.*;
+import java.io.*;
+
+class COMP108W04 {
+
+	// print the content of an array of size n
+	static void printArray(int[] data, int n) {
+		int i;
+
+		for (i=0; i < n; i++)
+			System.out.print(data[i] + " ");
+		System.out.println();
+	}
+
+	// Input: array1[] with size1 entries and array2[] with size2 entries
+	// print all entries of array1[] that does not exist in array2[]
+	static void notExists(int array1[], int size1, int array2[], int size2) {
+
+	}
+		
+	// Input: array1[] with size1 entries and array2[] with size2 entries
+	// for each entry in array2[], count how many times it appears in array1[]
+	static void count(int array1[], int size1, int array2[], int size2) {
+ 
+	}
+
+}
